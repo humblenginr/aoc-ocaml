d76d2a457d7fbc9b269763d1aab9aa475038bc6e
Nithish karthik <humblenginr@Nithishs-MacBook-Pro.local>
2023-09-20 18:42:37 +0530

    feat: Add solution for part 2
    Signed-off-by: Nithish karthik <humblenginr@Nithishs-MacBook-Pro.local>

--- bin/main.ml
+++ bin/main.ml
@@ -2,17 +2,12 @@
 (* X - Rock Y - Paper Z - Scissor*)
 (* A - Rock B - Paper C - Scissor*)
 
-let points_from_round my_value opponent_value = match my_value, opponent_value with  
-    | "X","A" ->  3
-    | "Y", "A" -> 6
-    | "Z", "A" -> 0
-    | "X", "B" -> 0
-    | "Y", "B" -> 3
-    | "Z", "B" -> 6
-    | "X", "C" -> 6
-    | "Y", "C" -> 0
-    | "Z", "C" -> 3
-    | _, _-> 0
+let points_for_result res = match res with  
+    | "X" ->  0
+    | "Y" -> 3
+    | "Z" -> 6
+    | _ -> assert false
+
 
  let points_for_shape = function 
     | "X" -> 1
@@ -20,7 +15,19 @@ let points_from_round my_value opponent_value = match my_value, opponent_value w
     | "Z" -> 3
     | _ -> assert false
 
-let total_points_for_round my_value opponent_value = points_for_shape my_value + points_from_round my_value opponent_value
+let get_my_value ov result = match ov, result with 
+  | "A", "X" -> "Z"
+  | "B", "X" -> "X"
+  | "C", "X" -> "Y"
+  | "A", "Y" -> "X"
+  | "B", "Y" -> "Y"
+  | "C", "Y" -> "Z"
+  | "A", "Z" -> "Y"
+  | "B", "Z" -> "Z"
+  | "C", "Z" -> "X"
+  | _, _ -> assert false
+
+let total_points_for_round my_value res = points_for_shape my_value + points_for_result res
 
   let read_lines filename = 
     let contents = In_channel.with_open_bin filename In_channel.input_all in 
@@ -32,8 +39,11 @@ let input = List.map (fun v -> String.split_on_char ' ' v) (read_lines "input.tx
 let rec total_points input_list total = match input_list with 
     | [] -> total
     | x :: xs -> match x with 
-      | ov :: mv :: _ -> total_points xs (total + total_points_for_round mv ov)
+      | ov :: res :: _ -> total_points xs (total + total_points_for_round (get_my_value ov res) res)
       | _ -> total
 
 let () = total_points input 0 |> string_of_int |> print_endline
+
+
+(* Part 2*)
 
