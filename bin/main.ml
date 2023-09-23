b7d3539b240394d50de8cf801e5d9e6d6d1d148a
Nithish karthik <humblenginr@Nithishs-MacBook-Pro.local>
2023-09-23 21:44:18 +0530

    feat: Add solution for part 2
    Signed-off-by: Nithish karthik <humblenginr@Nithishs-MacBook-Pro.local>

--- bin/main.ml
+++ bin/main.ml
@@ -7,7 +7,8 @@
       | x :: xs -> if (List.length res) > 2 then 
                         (* Make a hashtable out the list of last 3 elements of the `res` list and the upcoming element *)
                         (* If the length is 4, then it means that we have reached the end *)
-                        let hashtbl = Hashtbl.of_alist (module Char) (List.map (x :: (List.take res 3 )) ~f:(fun x -> (x, "ok"))) in
+                        (* 13 Characters for part 2 and 3 chars for part 1 *)
+                        let hashtbl = Hashtbl.of_alist (module Char) (List.map (x :: (List.take res 13 )) ~f:(fun x -> (x, "ok"))) in
                         match hashtbl with
                         | Hashtbl_intf.(`Duplicate_key _) -> solve xs (x :: res)
                         | Hashtbl_intf.(`Ok _) -> List.length (x :: res)
